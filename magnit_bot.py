60b413dc9502bc85c86d725cc7598de096c377db
stts <it.spectre.ru@gmail.com>
2022-07-01 13:05:36 +0500

    choise button

--- magnit_bot.py
+++ magnit_bot.py
@@ -7,7 +7,11 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-  await message.answer('Privet!')
+  start_buttons = ['Moscow', 'Orenburg']
+  keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  keyboard.add(*start_buttons)
+
+  await message.answer('Select a city', reply_markup=keyboard)
 
 
 if __name__ == '__main__':
